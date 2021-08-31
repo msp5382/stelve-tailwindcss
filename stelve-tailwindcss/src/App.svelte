f70429d3e114d8d53b5b83ae5641f7747fac8855
matcha <meen.sankaew@gmail.com>
2021-08-31 16:17:00 +0700

    feat: init

---
+++ stelve-tailwindcss/src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div class="max-w-lg w-11/12 mx-auto">
+    <div class="h-12">Vite Stelve Tailwind Typescript Boilerplate</div>
+  </div>
+</main>
---
+++ stelve-tailwindcss/src/main.ts
@@ -0,0 +1,7 @@
+import App from "./App.svelte";
+import "tailwindcss/dist/tailwind.css";
+const app = new App({
+  target: document.getElementById("app"),
+});
+
+export default app;
